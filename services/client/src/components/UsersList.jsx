3a9987683b1fd5324958eaa0a83633b39f7f8504
Nicholas Torba <nicholastorba@Nicholass-MacBook-Pro-3.local>
2018-10-15 15:19:06 -0400

    First react component additions

--- services/client/src/components/UsersList.jsx
+++ services/client/src/components/UsersList.jsx
@@ -6,10 +6,8 @@ const UsersList = (props) => {
     {
         props.users.map((user) => {
           return (
-            <h4
-              key={user.id}
-              className="box title is-4"
-            >{ user.username }
+            <h4 key={user.id} className="box title is-4">
+            { user.username }
             </h4>
           )
         })
